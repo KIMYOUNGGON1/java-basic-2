3e36f68837aedb5fa867f1ba07723355c28955f6
KIMYOUNGGON1 <dudrhs950@naver.com>
2024-08-26 09:01:48 +0900

    다형성 활용 및 추상클래스 & 메서드 시작

---
+++ src/poly/ex2/AnimalPolyMain3.java
@@ -0,0 +1,16 @@
+package poly.ex2;
+
+public class AnimalPolyMain3 {
+    public static void main(String[] args) {
+        Animal[] animalArr = {new Dog(), new Cat(), new Cow()};
+        soundAnimal(animalArr);
+    }
+
+    private static void soundAnimal(Animal[] animals) {
+        for (Animal animal : animals) {
+            System.out.println("동물 소리 테스트 시작");
+            animal.sound();
+            System.out.println("동물 소리 테스트 종료");
+        }
+    }
+}
