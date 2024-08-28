7e6fd6033b023ef38de1e14e9f507bbc979159f7
KIMYOUNGGON1 <dudrhs950@naver.com>
2024-08-28 09:36:21 +0900

    다형성 문제 풀이

---
+++ src/poly/ex/pay1/PayService.java
@@ -0,0 +1,31 @@
+package poly.ex.pay1;
+
+public class PayService {
+    public void processPay(String option, int amount) {
+
+        boolean result;
+        System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
+
+        result = payOption(option, amount);
+
+        if (result) {
+            System.out.println("결제가 성공했습니다.");
+        } else {
+            System.out.println("결제가 실패했습니다.");
+        }
+    }
+
+    private static boolean payOption(String option, int amount) {
+        boolean result;
+        if (option.equals("kakao")) {
+            Pay kakaoPay = new KakaoPay();
+            result = kakaoPay.pay(amount);
+        } else if (option.equals("naver")) {
+            Pay naverPay = new NaverPay();
+            result = naverPay.pay(amount);
+        } else {
+            System.out.println("결제 수단이 없습니다."); result = false;
+        }
+        return result;
+    }
+}
