8ceab95273a2b3f56dac60ca9d445ace39270f11
KIMYOUNGGON1 <dudrhs950@naver.com>
2024-08-28 12:38:32 +0900

    기본 강의 마무리 문제 풀이 완료

--- src/poly/ex/pay1/PayService.java
+++ src/poly/ex/pay1/PayService.java
@@ -2,11 +2,10 @@ package poly.ex.pay1;
 
 public class PayService {
     public void processPay(String option, int amount) {
-
-        boolean result;
         System.out.println("결제를 시작합니다: option=" + option + ", amount=" + amount);
 
-        result = payOption(option, amount);
+        Pay pay = PayStore.findPay(option);
+        boolean result = pay.pay(amount);
 
         if (result) {
             System.out.println("결제가 성공했습니다.");
@@ -16,16 +15,15 @@ public class PayService {
     }
 
     private static boolean payOption(String option, int amount) {
-        boolean result;
         if (option.equals("kakao")) {
             Pay kakaoPay = new KakaoPay();
-            result = kakaoPay.pay(amount);
+            return kakaoPay.pay(amount);
         } else if (option.equals("naver")) {
             Pay naverPay = new NaverPay();
-            result = naverPay.pay(amount);
+            return naverPay.pay(amount);
         } else {
-            System.out.println("결제 수단이 없습니다."); result = false;
+            Pay defaultPay = new DefaultPay();
+            return defaultPay.pay(amount);
         }
-        return result;
     }
 }
